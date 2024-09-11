d88cd82417f8465038ee109e1499b0a666aaed22
xolikov <a.xolikov@newuu.uz>
2024-09-11 14:07:42 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a - b - c << endl;
+    cout << b * c - a << endl;
+    cout << 2 * b - c + 5 * a << endl;
+    return 0;
+}
